3e908261b9da58850f4e67be0bc06d27add78643
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-03-15 17:39:11 -0400

    Changed name. Added function calls instead of library()

---
+++ inst/RScript/DiffExp_Stretches_v00.R
@@ -0,0 +1,134 @@
+
+args <- commandArgs(trailingOnly=TRUE)
+
+
+numIsoPerCluster <- data.table::fread(args[1])	## Need to make the package seamless
+					# enough that it takes the correct
+					#file depending on iso,tss,polyA
+
+comps <- c(args[2],args[4])
+
+type1 <- unlist(strsplit(args[3],","))
+type2 <- unlist(strsplit(args[5],","))
+
+numIsoPerCluster$V2[numIsoPerCluster$V2 %in% type1] = "Group1"
+numIsoPerCluster$V2[numIsoPerCluster$V2 %in% type2] = "Group2"
+comparisons <- c("Group1","Group2")
+
+inum <- as.integer(args[6])
+threshold <- as.integer(args[7])
+
+typeOfTest <- args[8]
+
+if(!dir.exists(paste0('TreeTraversal_',typeOfTest))){
+        dir.create(paste0('TreeTraversal_',typeOfTest))
+        out_dir <- paste0('TreeTraversal_',typeOfTest,'/',comps[1],"_",comps[2],"_",inum,"/")
+        dir.create(out_dir)
+} else {
+        out_dir <- paste0('TreeTraversal_',typeOfTest,'/',comps[1],"_",comps[2],"_",inum,"/")
+        dir.create(out_dir)}
+
+sink(paste0(out_dir,"REPORT_",threshold,".txt"), append=TRUE, split=TRUE)
+Sys.time()
+cat("Starting analysis in",comps[1],"vs",comps[2],"with atleast", threshold,"reads per gene \n")
+
+
+############################
+####### Functions ##########
+
+deltaPSI <- function(mat,gene,iso_to_cons){
+  pos <- sum(c(rev(mat$delta[mat$delta > 0]),0,0)[1:2])
+  neg <- abs(sum(c(mat$delta[mat$delta < 0],0,0)[1:2]))
+  change=max(pos,neg)
+  if (pos >= neg){
+    x = c(rev(mat$delta[mat$delta > 0]),0)[1:2]
+    return(change)}
+  else {
+    x = c(mat$delta[mat$delta < 0],0,0)[1:2]
+#    iso_to_cons[[gene]] <<- list(sapply(x[x!=0], function(value) mat$IsoID[mat$delta==value]))
+    return(-change)}
+}
+
+
+Get_Pval_DeltaPI <- function(mat){
+	max_ix <- as.integer(mat$IsoID[mat$delta == max(mat$delta)])
+	pval <- chisq.test(mat[,3:4])$p.value
+	d_psi <- deltaPSI(mat,gene,iso_to_cons)
+	return(list(pval,d_psi,max_ix))
+}
+
+############################
+
+coresToUse <- parallel::detectCores()/2
+
+
+numIsoPerCluster <- numIsoPerCluster[numIsoPerCluster$V2 %in% comparisons]
+colnames(numIsoPerCluster) <- c("Isoform","CellType","NumTranscripts")
+data_df <- separate(numIsoPerCluster, Isoform, into=c("Gene","IsoID"), sep="_")
+
+rm(numIsoPerCluster)
+
+
+data_df$IsoID <- as.integer(data_df$IsoID)
+data_df$IsoID[data_df$IsoID >= inum] <- inum
+
+
+processedDF = data_df %>% group_by(Gene, CellType, IsoID) %>%
+	summarise(Sum = sum(NumTranscripts)) %>%
+	filter(sum(Sum) >= 25 & length(Sum) >= 2) %>%
+	ungroup() %>% group_by(Gene) %>%
+	filter(length(unique(CellType)) == 2) %>%
+	tidyr::spread(CellType,Sum) %>% replace(is.na(.), 0) %>%
+	mutate(pi1 = Group1/sum(Group1),
+	pi2 = Group2/sum(Group2), delta = pi1-pi2) %>%
+	arrange(delta, .by_group = TRUE) %>% as.data.frame()
+
+PerGene = split(processedDF,processedDF$Gene)
+
+cat("About to start calculating p-values for",length(PerGene),"genes \n")
+
+
+
+uncorrected_output <- parallel::mclapply(names(PerGene),
+	function(geneName) Get_Pval_DeltaPI(PerGene[[geneName]]),mc.cores=coresToUse)
+names(uncorrected_output) <- names(PerGene)
+
+
+output_DF <- as.data.frame(do.call(rbind, uncorrected_output))
+colnames(output_DF) <- c("pvals","dPI","maxDeltaPI_ix")
+output_DF$FDR <- p.adjust(output_DF$pvals, method = "BH")
+output_DF$dPI <- as.numeric(output_DF$dPI)
+
+nums <- processedDF %>% select(Gene, IsoID, Group1, Group2) %>% group_by(Gene) %>%
+	arrange(IsoID, .by_group=TRUE) %>% as.data.frame()
+colnames(nums)[3:4] <- comps
+
+cat("Total genes tested:",nrow(output_DF),"\n")
+cat("Number of significant genes by fdr:",length(which(output_DF$FDR <= 0.05)),"\n")
+cat("Number of fdr genes with deltaPSI >= 0.1:",length(which(output_DF$FDR <= 0.05 & abs(output_DF$dPI) >= 0.1)),"\n")
+cat("Breakdown of isoform of sig genes with biggest change \n")
+sigs <- output_DF[which(output_DF$FDR <= 0.05 & abs(output_DF$dPI) >= 0.1),]
+print(table(unname(unlist(sigs$maxDeltaPI_ix))))
+cat('\n')
+Sys.time()
+sink()
+
+
+print("Writing output files")
+output_DF <- output_DF %>% tibble::rownames_to_column("Gene") %>% as.data.frame()
+output_DF <- as.matrix(output_DF)
+
+write.table(output_DF,
+            file=paste0(out_dir,comps[1],'_',comps[2],'_',threshold,'_results.csv'),
+            sep="\t",row.names = FALSE, quote = FALSE)
+
+write.table(nums,file = paste0(out_dir,"NumsForGenesBeingTested_",comps[1],"_",comps[2],"_",threshold),
+            quote = FALSE,row.names = FALSE)
+
+colnames(processedDF)[3:4] <- comps
+save(processedDF,file=
+       paste0(out_dir,comps[1],'_',comps[2],'_',inum,'X2_Atleast',threshold,'reads.Robj'))
+
+
+sig_Genes <- rownames(sigs)
+write.table(sig_Genes,file = paste0(out_dir,'sigGenes_',comps[1],'_',comps[2],threshold),quote=FALSE,row.names = FALSE)
