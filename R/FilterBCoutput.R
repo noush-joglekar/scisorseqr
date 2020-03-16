d3a639093a34e852dc8f6c0be2b3046dfd152d11
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-03-16 16:50:32 -0400

    Separated filtering function to script

---
+++ R/FilterBCoutput.R
@@ -0,0 +1,26 @@
+#' Filter unbarcoded reads
+#'
+#' @description This function filters out all the reads that don't have a barcode
+#' and outputs them into a separate folder.
+#' Option to concatenate output from several files if they belong to
+#' the same sample.
+#' @param outputFolder automatically sourced from GetBarcodes script
+#' @param raw_Output unfiltered output of GetBarcodes script
+#' @param concatenate Combines output of all fastq.gz files if they belong
+#' to the same sample. Defaults to TRUE
+#' @export
+
+FilterBCOutput <- function(outputFolder, raw_Output,  concatenate = TRUE) {
+  filt_Output <- paste0(outputFolder, "/OutputFiltered/")
+  dir.create(filt_Output)
+
+  # Maybe can add feature to randomize selection of one read per barcode-UMI pair
+  # code in KLa's folder
+  if (concatenate == TRUE){
+    filterAndConcat <- paste("sh", filterOut, raw_Output, filt_Output, TRUE)
+    system(filterAndConcat)
+  } else {
+    filterOnly <- paste("sh", filterOut, raw_Output, filt_Output, FALSE)
+    system(filterOnly)
+  }
+}
