ada04bd104cee343bc5e5dd059d854701934ca40
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-03-16 16:35:12 -0400

    added conditional statements (hierarchy)

--- R/DiffSplicingAnalysis.R
+++ R/DiffSplicingAnalysis.R
@@ -14,14 +14,37 @@
 #' depending on input. Defaults to Iso
 #' @return Output folder with results for each line of config
 #' file, i.e. for each set of comparisons
-#' @return Result file with gene, FDR, deltaPI value
+#' @return Result file with gene, uncorrected pval, deltaPI, FDR
+#' @return handy .Robj per comparison to import if need be
+#' @return file with significantly differentially spliced genes
+#' @return file with numbers per gene and isoform tested in each group
 #' @usage DiffSplicingAnalysis('config',10,25,'Exon')
 #'
 #' @export
-DiffSplicingAnalysis <- function(configFile, numIsoforms = 10, minNumReads = 25, typeOfTest = "Iso") {
+DiffSplicingAnalysis <- function(configFile, numIsoforms = 10, minNumReads = 25, typeOfTest = "Iso",
+                                 is.hier = FALSE, region1 = NULL, region2 = NULL, yamlFile = NULL) {
   R_file <- system.file("RScript", "DiffExp_Stretches_v00.R", package = "scisorseqr")
-  call1 <- paste0("while read -r line; do echo $line | Rscript ", R_file,
-                  " IsoQuantOutput/NumIsoPerCluster $(awk \'{split($0,a,\"\\t\"); {print a[1],a[2],a[3],a[4]}}\') ",
-                  numIsoforms, " ", minNumReads," ", typeOfTest, " ; done < ", configFile)
+
+  if (typeOfTest == "Iso"){
+    inputFile = "IsoQuantOutput/NumIsoPerCluster"
+  } else if (typeOfTest == "TSS"){
+    inputFile = "IsoQuantOutput/NumIsoPerCluster"
+  } else if (typeOfTest == "PolyA"){
+    inputFile = "IsoQuantOutput/NumIsoPerCluster"
+  } else if (typeOfTest == "Exon"){
+    inputFile = "ExonInclusionExclusion.tsv"
+  }
+
+  if (is.hier == FALSE){
+    call1 <- paste("while read -r line; do echo $line | Rscript", R_file,
+                   inputFile, "$(awk \'{split($0,a,\"\\t\"); {print a[1],a[2],a[3],a[4]}}\')",
+                   numIsoforms, minNumReads, typeOfTest, FALSE, "; done <", configFile)
+  } else {
+    call1 <- paste("while read -r line; do echo $line | Rscript ", R_file,
+                   inputFile, "$(awk \'{split($0,a,\"\\t\"); {print a[1],a[2],a[3],a[4]}}\')",
+                   numIsoforms, minNumReads, typeOfTest, TRUE,
+                   yamlFile, region1, region2, "; done <", configFile)
+  }
+
   system(call1)
 }
