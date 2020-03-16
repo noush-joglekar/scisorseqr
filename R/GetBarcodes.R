4c43044ccbae8c76f4f3d176dc617288783067cd
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-03-16 16:50:11 -0400

    Re-routed filtering function to separate script

--- R/GetBarcodes.R
+++ R/GetBarcodes.R
@@ -45,25 +45,6 @@ GetBarcodes <- function(fqFolder, BCClustAssignFile, outputFolder = ".", numProc
 
   # Calls next function automatically once all files have been analyzed and
   # .csv files created.
-  MaptoCellType(outputFolder, raw_Output, concatenate = TRUE)
+  scisorseqr::FilterBCOutput(outputFolder, raw_Output, concatenate = TRUE)
 }
 
-#' @export
-#' This function filters out all the reads that don't have a barcode
-#' and outputs them into a separate folder.
-#' Option to concatenate output from several files if they belong to
-#' the same sample.
-MaptoCellType <- function(outputFolder, raw_Output,  concatenate = TRUE) {
-  filt_Output <- paste0(outputFolder, "/OutputFiltered/")
-  dir.create(filt_Output)
-
-  # Maybe can add feature to randomize selection of one read per barcode-UMI pair
-  # code in KLa's folder
-  if (concatenate == "TRUE"){
-    filterAndConcat <- paste("sh", filterOut, raw_Output, filt_Output, TRUE)
-    system(filterAndConcat)
-  } else {
-    filterOnly <- paste("sh", filterOut, raw_Output, filt_Output)
-    system(filterOnly)
-  }
-}
