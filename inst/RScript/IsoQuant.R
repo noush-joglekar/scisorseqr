c45e38acf0d871b04630d54affca65ad51c6d65a
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-03-19 16:00:56 -0400

    Fixed the duplication issue

--- inst/RScript/IsoQuant.R
+++ inst/RScript/IsoQuant.R
@@ -1,5 +1,3 @@
-### Need to figure out why I'm getting duplicated lines
-
 args <- commandArgs(trailingOnly=TRUE)
 
 
@@ -19,8 +17,7 @@ if(is.tss != FALSE || is.polya != FALSE ) {
 
 if(is.iso != FALSE){
 	ids1 <- all_info %>% group_by(Gene) %>% count(Isoform) %>% arrange(-n, .by_group=TRUE) %>% mutate(iso.id = row_number()-1)
-	df1 <- inner_join(all_info,ids1, by = "Isoform") %>% select(-Gene.y, -n, -Isoform)
-	colnames(df1)[2] <- 'Gene'
+	df1 <- inner_join(all_info,ids1, by = c("Gene","Isoform")) %>% select(-n, -Isoform)
 	write.table(ids1,file=paste0(isoDir,"Iso-IsoID.csv"),col.names=TRUE,row.names=FALSE,quote=FALSE,sep="\t")
 }
 
@@ -28,8 +25,7 @@ if(is.iso != FALSE){
 
 if(is.tss != FALSE){
 	ids2 <- data_df %>% group_by(Gene) %>% count(TSS) %>% arrange(-n, .by_group=TRUE) %>% mutate(tss.id = row_number()-1)
-	data_df <- inner_join(data_df,ids2, by = "TSS") %>% select(-Gene.y, -n, -TSS)
-	colnames(data_df)[2] <- 'Gene'
+	data_df <- inner_join(data_df,ids2, by = c("Gene","TSS")) %>% select(-n, -TSS)
 	write.table(ids2,file=paste0(isoDir,"TSS-ID.csv"),col.names=TRUE,row.names=FALSE,quote=FALSE,sep="\t")
 }
 
@@ -37,8 +33,7 @@ if(is.tss != FALSE){
 
 if(is.polya != FALSE){
 	ids3 <- data_df %>% group_by(Gene) %>% count(PolyA) %>% arrange(-n, .by_group=TRUE) %>% mutate(polya.id = row_number()-1)
-	data_df <- inner_join(data_df,ids3, by = "PolyA") %>% select(-Gene.y, -n, -PolyA)
-	colnames(data_df)[2] <- 'Gene'
+	data_df <- inner_join(data_df,ids3, by = c("Gene","PolyA")) %>% select(-n, -PolyA)
 	write.table(ids3,file=paste0(isoDir,"PolyA-PolyID.csv"),col.names=TRUE,row.names=FALSE,quote=FALSE,sep="\t")
 }
 
