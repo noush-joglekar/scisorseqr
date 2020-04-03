02d8e643499b7777e573d7f26c18af9b6e8c9cf4
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-04-03 12:13:38 -0400

    Space after comma messes up FS

--- inst/bash/concat_ParallelizedBC.sh
+++ inst/bash/concat_ParallelizedBC.sh
@@ -3,14 +3,14 @@
 f_name=$1;
 outFolder=$2;
 
-cat $outFolder/tmp* | tr -d "[']" > $outFolder/"DeconvBC_"$f_name".csv"
+cat $outFolder/tmp* | tr -d "[ ']" > $outFolder/"DeconvBC_"$f_name".csv"
 rm $outFolder/tmp*
 
 cat $outFolder/"DeconvBC_"$f_name".csv" | awk '{tot++1;polyA[$2]++;totLen+=$11;\
 if($5!~/no_bc|,/){bc++1;len+=$11;};if(!(see[$5])) {see[$5]++1;count++1;}; tso[$9]++1;}END \
 {print "Total reads >= 200 bp:",tot; \
 for(p in polyA) {print p":",polyA[p],"\nPercent",p":",polyA[p]/tot}; \
-print "Unique Barcodes:", count-1;
+print "Unique Barcodes:", count-2;
 print "Reads with barcode:",bc; \
 print "Reads with barcode given polyA:", bc/polyA["poly_T_found"]; \
 print "Percent barcoded reads:", bc/tot; \
