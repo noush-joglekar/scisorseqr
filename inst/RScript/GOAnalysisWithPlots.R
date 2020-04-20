fc472c48eae56a94d2f47f1bda93416bc5b672fc
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-04-20 13:19:33 -0400

    GO function. Contains only MF and BP for now

---
+++ inst/RScript/GOAnalysisWithPlots.R
@@ -0,0 +1,61 @@
+args <- commandArgs(trailingOnly=TRUE)
+current_dir <- args[1]
+setwd(current_dir)
+
+pairwise_dir <- args[2]
+
+files = list.files(pairwise_dir,recursive = TRUE)[grep(
+  pattern = "results.csv",list.files(pairwise_dir,recursive = TRUE))]
+
+
+mf <- list()
+bp <- list()
+
+dir.create('OutputGO/GO_BP/')
+dir.create('OutputGO/GO_MF/')
+
+for (file in files){
+  name = strsplit(file,"_10")[[1]][1]
+  print(paste("Conducting GO analysis against",name))
+  result_file <- read.table(paste0(pairwise_dir,file),sep="\t", header=TRUE)
+  sig_ix <- which(result_file$FDR <= 0.05 & abs(result_file$dPI)>=0.1)
+  sigGenes <- unlist(strsplit(as.vector(result_file$Gene[sig_ix]),"[.]"))[c(TRUE,FALSE)]
+  ego_MF <- clusterProfiler::enrichGO(sigGenes,OrgDb = org.Mm.eg.db,keyType= 'ENSEMBL',ont = "MF")
+  ego_BP <- clusterProfiler::enrichGO(sigGenes,OrgDb = org.Mm.eg.db,keyType= 'ENSEMBL',ont = "BP")
+
+  mf[name] <- ego_MF
+  bp[name] <- ego_BP
+
+  rm(ego_MF)
+  rm(ego_BP)
+}
+
+printMF <- function(mf,name,file){
+  mf1 <- enrichplot::emapplot(mf[[name]], vertex.label.cex=1.2)
+
+  pdf(file = paste0("OutputGO/GO_MF/",name,".pdf"),12,10)
+  print(mf1)
+  dev.off()
+}
+
+printBP <- function(bp,name,file){
+  bp1 <- enrichplot::emapplot(bp[[name]], vertex.label.cex=1.2)
+
+  pdf(file = paste0("OutputGO/GO_BP/",name,".pdf"),12,10)
+  print(bp1)
+  dev.off()
+}
+
+for (file in files){
+  name = strsplit(file,"_10")[[1]][1]
+  print(name)
+  try(printMF(mf,name,file))
+  try(printBP(bp,name,file))
+}
+
+save(mf,file='OutputGO/GO_MF/MolecularFunction.Robj')
+save(bp,file='OutputGO/GO_BP/BiologicalProcess.Robj')
+
+print("GO plots in OutputGO directory under GO_MF and GO_BP")
+
+
