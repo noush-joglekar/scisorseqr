199e007259fbb529f387284155add32ffa4a96c8
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-07-13 16:20:33 -0400

    GO with cc

--- inst/RScript/GOAnalysisWithPlots.R
+++ inst/RScript/GOAnalysisWithPlots.R
@@ -10,9 +10,11 @@ files = list.files(pairwise_dir,recursive = TRUE)[grep(
 
 mf <- list()
 bp <- list()
+cc <- list()
 
 dir.create('OutputGO/GO_BP/')
 dir.create('OutputGO/GO_MF/')
+dir.create('OutputGO/GO_CC/')
 
 for (file in files){
   name = strsplit(file,"_10")[[1]][1]
@@ -22,12 +24,15 @@ for (file in files){
   sigGenes <- unlist(strsplit(as.vector(result_file$Gene[sig_ix]),"[.]"))[c(TRUE,FALSE)]
   ego_MF <- clusterProfiler::enrichGO(sigGenes,OrgDb = org.Mm.eg.db,keyType= 'ENSEMBL',ont = "MF")
   ego_BP <- clusterProfiler::enrichGO(sigGenes,OrgDb = org.Mm.eg.db,keyType= 'ENSEMBL',ont = "BP")
+  ego_CC <- clusterProfiler::enrichGO(sigGenes,OrgDb = org.Mm.eg.db,keyType= 'ENSEMBL',ont = "CCC")
 
   mf[name] <- ego_MF
   bp[name] <- ego_BP
+  cc[name] <- ego_CC
 
   rm(ego_MF)
   rm(ego_BP)
+  rm(ego_CC)
 }
 
 printMF <- function(mf,name,file){
@@ -46,15 +51,26 @@ printBP <- function(bp,name,file){
   dev.off()
 }
 
+printBP <- function(cc,name,file){
+  cc1 <- enrichplot::emapplot(cc[[name]], vertex.label.cex=1.2)
+
+  pdf(file = paste0("OutputGO/GO_CC/",name,".pdf"),12,10)
+  print(cc1)
+  dev.off()
+}
+
 for (file in files){
   name = strsplit(file,"_10")[[1]][1]
   print(name)
   try(printMF(mf,name,file))
   try(printBP(bp,name,file))
+  try(printCC(cc,name,file))
+
 }
 
 save(mf,file='OutputGO/GO_MF/MolecularFunction.Robj')
 save(bp,file='OutputGO/GO_BP/BiologicalProcess.Robj')
+save(cc,file='OutputGO/GO_CC/CellularComponent.Robj')
 
 print("GO plots in OutputGO directory under GO_MF and GO_BP")
 
