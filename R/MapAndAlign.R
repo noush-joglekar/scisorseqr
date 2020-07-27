6083a9ef662448fdf65da7eab02d4851f3b2e706
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-07-27 14:52:35 -0400

    Added cage-polyA filtering

--- R/MapAndAlign.R
+++ R/MapAndAlign.R
@@ -8,20 +8,35 @@
 #' @param outputDir location within the working directory for files
 #' that will be created during this process
 #' @param annoGZ annotation.gtf.gz file. Defaults to v21 mm10
-#' @param chromFaDir directory containing fa.gz files by individual
-#' chromosme. Defaults to mm10
 #' @param numThreads number of threads for parallel processes. Defaults to 12
+#' @param filterFullLength logical indicating whether reads should be filtered
+#' for having start and end sites falling into annotated cage peaks or polyA
+#' sites. Defaults to FALSE
+#' @param cageBed bed.gz file containing annotated CAGE peaks
+#' @param polyABed bed.gz file containing annotated polyA sites
+#' @param cp_distance distance from annotated CAGE peak or polyA site
+#' for a read to be considered full-length. Defaults to 50
 #' @return directory containing several bam, gff.gz, and flat files
 #' necessary for downstream analysis
-#' @usage MapAndAlign('LRoutput','gencode.vM21.annotation.gtf.gz','chromFa/',16)
+#' @usage MapAndAlign('LRoutput','gencode.vM21.annotation.gtf.gz',16)
 #' @export
 #'
-MapAndAlign <- function(outputDir = 'LRProcessingOutput/', annoGZ, chromFaDir, numThreads = 12){
+MapAndAlign <- function(outputDir = 'LRProcessingOutput/', annoGZ, numThreads = 12,
+                        filterFullLength = FALSE, cageBed = NULL, polyABed = NULL,
+                        cp_distance = 50){
 
   checkFile <- system.file("bash", "toolCheck.sh", package = "scisorseqr")
   if(system(paste("sh", checkFile)) == 127) {
     stop(paste0("Error: samtools necessary for conversion to .bam format \n",
                 "Check that both bedtools and samtools are installed and loaded before moving forward."))
+  }
+
+  if(filterFullLength == TRUE){
+    if(is.null(cageBed) || is.null(polyABed)){
+      stop("Please provide bed.gz files for CAGE and PolyA sites")
+    } else if (grepl(".gz",cageBed) == FALSE || grepl(".gz",polyABed) == FALSE){
+      stop("Please provide bed.gz files for CAGE and PolyA sites")
+    }
   }
 
   miscFolder <- 'Misc/'
@@ -40,5 +55,13 @@ MapAndAlign <- function(outputDir = 'LRProcessingOutput/', annoGZ, chromFaDir, n
 
   runCommand <- paste("sh", mainFile, fastqGuide, outputDir, tmpFolder, numThreads,
                       chromFaDir, annoGZ, bamGuide, awkScriptDir)
+
+  if(filterFullLength == TRUE){
+    cageGZ <- cageBed
+    polyaGZ <- polyABed
+    filterFL_script <- system.file("bash", "cagePolyA.sh", package = "scisorseqr")
+    filterFLcomm <- paste("sh", filterFL_script, "zcat", outputDir,
+                          cageGZ, polyaGZ, awkScriptDir, cp_distance)
+  }
 
 }
