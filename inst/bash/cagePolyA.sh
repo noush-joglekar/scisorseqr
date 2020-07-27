14c9b40d7f95d72bb67e99280c6969c480bc55a7
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-07-27 15:01:21 -0400

    added cage-polya filtering scripts

---
+++ inst/bash/cagePolyA.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# by Hagen Tilgner for mapping of 454/pacBio.MOLECULO reads : 5/2014
+
+function checkForFile {
+    file=$1;
+    if [ ! -f $file ]
+    then
+       echo "ERROR:"$file" is not a regular file ... exiting "
+       exit;
+    fi
+}
+
+function checkForDirectory {
+    dir=$1;
+    if [ ! -d $dir ]
+    then
+       echo "ERROR:"$dir" is not a regular directory ... exiting "
+       exit;
+    fi
+}
+
+###############
+# 0. the arguments
+echo "+++++++++++++++++++++++++ 1. arguments";
+
+echo "++++++++++++++++++ 1a. flexible";
+unzipCommand=${1}
+echo "unzipCommand="$unzipCommand
+
+inputDir=${2}
+outputDir=$inputDir
+echo "inputDir=outputDir="$inputDir
+checkForDirectory $inputDir
+
+CAGEBED=${3}
+echo "CAGEBED="$CAGEBED
+checkForFile $CAGEBED
+
+POLYABED=${4}
+echo "POLYABED="$POLYABED
+checkForFile $POLYABED
+
+scriptDir=${5}
+echo "scriptDir="$scriptDir
+checkForDirectory $scriptDir
+
+distance=${6}
+echo "cpDistance="$distance
+
+echo "+++++++++++++++++++++++++ 2. full-length analysis";
+echo "++++++++++++++++++++++ 2a. CAGE";
+echo "+++++++++++++++++++ 2a.1 execution";
+
+awk -v exonMapping=$inputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.gff.gz \
+-v readsVsGenes=$inputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.transcriptWise.genes.gz \
+-v anno=$CAGEBED -v unzipCommand=$unzipCommand -v cageCutoff=1 -v verbose=2 \
+-f ${scriptDir}/v2.1a.compareTranscriptStarts_vs_CageTSS.awk | gzip -c > \
+$outputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.cageRecordTSS.tab.gz
+
+echo "+++++++++++++++++++ 2a.2 stats";
+for i in 50 45 40 35 30 25 20 15 10 5; do n=`$unzipCommand \
+$outputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.cageRecordTSS.tab.gz \
+| awk -v c=$i '$2<=c || $4<=c' | wc -l`; m=`$unzipCommand \
+$outputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.cageRecordTSS.tab.gz \
+| wc -l`; o=`awk -v m=$m -v n=$n 'BEGIN{print n/m;}'`; \
+echo -e $i"\t"$n"\t"$o; done
+
+echo "++++++++++++++++++++++ 2b. polyA";
+echo "+++++++++++++++++++ 2b.1 execution";
+awk -v exonMapping=$inputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.gff.gz \
+-v readsVsGenes=$inputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.transcriptWise.genes.gz \
+-v anno=$POLYABED -v unzipCommand=$unzipCommand -v cageCutoff=0.0001 -v verbose=2 \
+-f ${scriptDir}/v2.1a.compareTranscriptEnds_vs_PolyAs.awk | gzip -c > \
+$outputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.polyAsiteRecordPA.tab.gz
+
+echo "+++++++++++++++++++ 2b.2 stats";
+for i in 50 45 40 35 30 25 20 15 10 5; do n=`$unzipCommand \
+$outputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.polyAsiteRecordPA.tab.gz | \
+awk -v c=$i '$2<=c || $4<=c' | wc -l`; m=`$unzipCommand \
+$outputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.polyAsiteRecordPA.tab.gz | \
+wc -l`; o=`awk -v m=$m -v n=$n 'BEGIN{print n/m;}'`; echo -e $i"\t"$n"\t"$o; done
+
+
+echo "+++++++++++++++++++++++++ 3. getting 5' and 3' complete files";
+echo "++++++++++++++++++++++ 3.a stretches";
+
+awk -v unzipCommand=$unzipCommand -v inputDir=$inputDir -v distance=$distance \
+-v outputDir=$outputDir 'BEGIN{comm=unzipCommand" "\
+outputDir"/mapping.bestperRead.RNAdirection.withConsensIntrons.cageRecordTSS.tab.gz"; \
+while(comm|getline){d=1000; if(substr($1,1,1)=="#"){continue;} \
+if($2<=distance){d=$2; TSS[$1]=$3; }  if($4<=distance && $4<d){TSS[$1]=$5;}}
+comm=unzipCommand" "outputDir"/mapping.bestperRead.RNAdirection.withConsensIntrons.polyAsiteRecordPA.tab.gz"; \
+while(comm|getline){d=1000; if(substr($1,1,1)=="#"){continue;} \
+if($4<=distance){d=$4; polyA[$1]=$5;}  if($2<=distance && $2<d){polyA[$1]=$3;}} \
+for(k in TSS){if(k in polyA){r2r[k]=k;}} \
+comm=unzipCommand" "inputDir"/newIsoforms_vs_Anno_ignoreAnno/stretches.gz"; OFS="\t"; \
+while(comm|getline){split($3,a,/\=|\@/); \
+if(a[2] in r2r){print $1,$2,a[1]"="r2r[a[2]]"@"a[3],$4,TSS[r2r[a[2]]], polyA[r2r[a[2]]]; }}}' | \
+gzip -c > $outputDir/newIsoforms_vs_Anno_ignoreAnno/CagePolyA.complete.stretches.gz
+
+echo "++++++++++++++++++++++ 3.b transcriptWiseGenes";
+$unzipCommand $inputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.transcriptWise.genes.gz | \
+awk -v unzipCommand=$unzipCommand -v outputDir=$outputDir \
+'BEGIN{comm=unzipCommand" "outputDir"/newIsoforms_vs_Anno_ignoreAnno/CagePolyA.complete.stretches.gz"; \
+while(comm|getline){split($3,a,/\=|\@/); use[a[2]]=1;}}{if($1 in use){print $0;}}' | gzip -c > \
+$outputDir/CagePolyA.complete.mapping.bestperRead.RNAdirection.withConsensIntrons.transcriptWise.genes.gz
+
+echo "++++++++++++++++++++++ 3.c mapping file";
+$unzipCommand $inputDir/mapping.bestperRead.RNAdirection.withConsensIntrons.gff.gz | \
+awk -v unzipCommand=$unzipCommand -v outputDir=$outputDir \
+'BEGIN{comm=unzipCommand" "outputDir"/newIsoforms_vs_Anno_ignoreAnno/CagePolyA.complete.stretches.gz"; \
+while(comm|getline){split($3,a,/\=|\@/); use[a[2]]=1;}}{split($10,a,"\""); if(a[2] in use){print $0;}}' | gzip -c > \
+$outputDir/CagePolyA.complete.mapping.bestperRead.RNAdirection.withConsensIntrons.gff.gz
+
+echo "+++++++++++++++++++++++++ 3. done ";
+exit;
---
+++ inst/bash/v2.1a.compareTranscriptEnds_vs_PolyAs.awk
@@ -0,0 +1,173 @@
+# goal: remove polyA-tails and remove revcomp sequence to RNA if necessary
+# by Hagen;
+
+# exampel of ccs read data: 
+#nuvol:~ htilgner$ gzcat /Users/htilgner/data/trios/input/pacBioIsoforms/v1/yorubian/Gm19238/round1/ccs/fa/m120413_003026_00126_c100277812550000001523007807041240_s2_p0.ccs.fasta.gz | head -2
+#>m120413_003026_00126_c100277812550000001523007807041240_s2_p0/57/ccs
+#TTTTTTTTGAAGGTTCTCAGGTCTTTATTTGCTCTCTCAACTTCCAGGAATTGACTTATTTAATTAATCC
+
+# exampel of subread data
+#nuvol:~ htilgner$ gzcat /Users/htilgner/data/trios/input/pacBioIsoforms/v1/yorubian/Gm19238/round1/subreads/fa/GM19238_poly-AcDNA_opt_smrtanalysis_common_jobs_016_016525_data_filtered_subreads.fasta.gz| head -2
+#>m120527_035935_00126_c100333442550000001523018909161283_s1_p0/7/0_334
+#TTTGCGACCTTTCGCCACGACGTCAGCGCGCTTTCCCGGGCAGACGCCTC
+
+
+function max(a,b){
+    if(a>b){return(a);}
+    return(b);
+}
+function min(a,b){
+    if(a<b){return(a);}
+    return(b);
+}
+function allOptionsThere(){  
+    if(!exonMapping){
+	print "ERROR: -v exonMapping=<string> must be provided; exiting" > "/dev/stderr";
+	exit;
+    }
+    if(!readsVsGenes){
+	print "ERROR: -v readsVsGenes=<string> must be provided; exiting" > "/dev/stderr";
+	exit;
+    }
+    if(!anno){
+	print "ERROR: -v anno=<string> must be provided; exiting" > "/dev/stderr";
+	exit;
+    }
+    # from when on do we actually use
+    if(!(cageCutoff)){
+	print "ERROR: -v cageCutoff must be provided;  exiting" > "/dev/stderr";
+	exit;	
+    }
+    if(!(unzipCommand)){
+	print "ERROR: -v unzipCommand must be provided;  exiting" > "/dev/stderr";
+	exit;	
+    }
+
+}
+
+function comp(x){
+    x=toupper(x) ;
+    if(x=="A"){return("T");}
+    if(x=="T"){return("A");} 
+    if(x=="C"){return("G");} 
+    if(x=="G"){return("C");} 
+    if(x=="N"){return("N");}   
+    
+    print "ERROR:"x" is not a vlaid symbol" > "/dev/stderr";
+    exit(0);
+} 
+
+function revcomp(x){
+    y=""; 
+    for(j=length(x);j>=1;j--){
+	y=y""comp(substr(x,j,1));
+    } 
+    return(y);
+}
+
+function abs(x){
+    if(x<0){x=(-1)*x;} 
+    return(x);
+}
+
+
+BEGIN{
+
+    # need to have the required options
+    allOptionsThere();
+    OFS="\t";
+    if(verbose>=1)
+	print "# 1. reading annotation" > "/dev/stderr";
+
+    comm=unzipCommand" "anno;
+    while(comm|getline){
+	
+	if($5>=cageCutoff){
+	    xP1=$2+1;
+	    for(i=$2+1;i<=$3;i++)
+		polyA[$1"_"i"_"$6]=$1"_"xP1"_"$3"_"$6;
+	}
+    } 
+
+    if(verbose>=1)
+	print "# 2. reading readsVsGenes" > "/dev/stderr";
+  
+    comm=unzipCommand" "readsVsGenes; 
+    while(comm | getline){
+	if($2=="none" || $3~/problem/ || $4~/problem/){skippedReads[$1]=1; continue;}
+	read2Gene[$1]="\""$2"\";";
+    }
+
+    if(verbose>=1)
+	print "# 3. reading exonMapping" > "/dev/stderr";
+
+    comm=unzipCommand" "exonMapping;
+    while(comm|getline){
+	split($10,a,"\""); 
+	r=a[2]; 
+	if(!(r in read2Gene)){if(!(r in skippedReads)){print "ERROR"r > "/dev/stderr"; exit;}else{continue;}}    
+	if(r in read2ReadEnd){
+	    if($7=="+"){read2ReadEnd[r]=max(read2ReadEnd[r],$5);}
+	    if($7=="-"){read2ReadEnd[r]=min(read2ReadEnd[r],$4);}   
+	}
+	else{
+	    if($7=="+"){read2ReadEnd[r]=$5;}
+	    if($7=="-"){read2ReadEnd[r]=$4;}
+	    readStrand[r]=$7;
+	    readChrom[r]=$1;
+	} 
+    }
+
+    if(verbose>=1)
+	print "# 4. finding the closest polyA" > "/dev/stderr";
+    
+    readCounter=0;
+    print "#readID\tminDUpstream\tminDUpstreamPolyA\tminDUpstream\tminDUpstreamPolyA\tGene\treadChrom\treadStrand"; 
+    for(r in read2ReadEnd){
+	readCounter++;
+	if(readCounter % 1000000 == 0 && verbose>=2){
+	    print readCounter > "/dev/stderr";
+	}
+	if(!(r in read2Gene && r in readStrand)){print "ERROR2" > "/dev/stderr"; exit;}
+	minDDownstream=1000000000;
+	minDDownstreampolyA="none--";
+
+	minDUpstream=1000000000;
+	minDUpstreampolyA="none--";
+	
+	polyA["none--"]="none--";
+        #print r"\t"read2ReadEnd[r];
+	#for(s in polyA){
+	#    print "polyA\t"s;
+	#}
+	
+	for(i=read2ReadEnd[r]-50;i<=read2ReadEnd[r]+50;i++){
+	    s=readChrom[r]"_"i"_"readStrand[r];
+	    if(s in polyA){
+		
+		# closest upstream
+		if(readStrand[r]=="+" && i<=read2ReadEnd[r] && abs(read2ReadEnd[r]-i)<minDUpstream){
+		    minDUpstream=abs(read2ReadEnd[r]-i);
+		    minDUpstreampolyA=s;			     
+		}
+		if(readStrand[r]=="-" && i>=read2ReadEnd[r] && abs(read2ReadEnd[r]-i)<minDUpstream){
+		    minDUpstream=abs(read2ReadEnd[r]-i);
+		    minDUpstreampolyA=s;			     
+		}
+		# closest downstream
+		if(readStrand[r]=="+" && i>=read2ReadEnd[r] && abs(read2ReadEnd[r]-i)<minDDownstream){
+		    minDDownstream=abs(read2ReadEnd[r]-i);
+		    minDDownstreampolyA=s;	
+		}		
+		if(readStrand[r]=="-" && i<=read2ReadEnd[r] && abs(read2ReadEnd[r]-i)<minDDownstream){
+		    minDDownstream=abs(read2ReadEnd[r]-i);
+		    minDDownstreampolyA=s;	
+		}
+
+	    }	    
+	}
+	print r"\t"minDUpstream"\t"polyA[minDUpstreampolyA]"\t"minDDownstream"\t"polyA[minDDownstreampolyA]"\t"read2Gene[r]"\t"readChrom[r]"\t"readStrand[r];          
+    }
+}
+
+ 
