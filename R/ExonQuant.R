42d929627acecf5c1e978a1b669ed3108bdd7658
Anoushka Joglekar <anj2026@med.cornell.edu>
2020-08-19 15:11:11 -0400

    isoquant

--- R/ExonQuant.R
+++ R/ExonQuant.R
@@ -3,6 +3,9 @@
 #' @description This function groups reads by either the celltype
 #' or cell-barcode to count inclusion and exclusion of exons. Inputs
 #' to this function are based on previous preprocessing steps
+#'
+#' For ONT data we recomment \url{https://github.com/ablab/IsoQuant.git}
+#' which allows for non-exact splice-site matching
 #' @param allInfoFile file containing cell barcode and celltype information
 #' per read. Defaults to output of the InfoPerLongRead function
 #' @param exon.gff gzipped file containing exon mapping information.
@@ -11,7 +14,8 @@
 #' counting inclusion levels. Defaults to celltype
 #' @param threshold minimum number of reads per grouping factor in order
 #' to consider that exon to be sufficiently expressed. Defaults to 10
-#' @seealso \code{\link{MapAndFilter}} \code{\link{InfoPerLongRead}}
+#' @seealso \code{\link{MapAndFilter}}
+#' @seealso \code{\link{InfoPerLongRead}}
 #' @return ExonQuantOutput/InclusionExclusionCounts.tsv
 #' @export
 #'
