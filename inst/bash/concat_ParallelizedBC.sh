35a0f6fe8f38a7f217f4abfa614cbcb170bcf898
Anoushka <anj2026@med.cornell.edu>
2021-02-16 15:26:11 -0500

    summary % mult by 100

--- inst/bash/concat_ParallelizedBC.sh
+++ inst/bash/concat_ParallelizedBC.sh
@@ -10,12 +10,21 @@ rm $outFolder/tmp*
 cat $outFolder/"DeconvBC_"$f_name".csv" | awk '{tot++1;polyA[$2]++;totLen+=$11;\
 if($5!~/no_bc|,/){bc++1;len+=$11;};if(!(see[$5])) {see[$5]++1;count++1;}; tso[$9]++1;}END \
 {print "Total reads >= 200 bp:",tot; \
-for(p in polyA) {print p":",polyA[p],"\nPercent",p":",polyA[p]/tot}; \
+for(p in polyA) {print p":",polyA[p],"\nPercent",p":",polyA[p]*100/tot}; \
 print "Unique Barcodes:", count-2;
 print "Reads with barcode:",bc; \
-print "Reads with barcode given polyA:", bc/polyA["poly_T_found"]; \
-print "Percent barcoded reads:", bc/tot; \
-print "Percent TSO found:",tso["TSO_found"]/tot; \
-print "Percent doubleTSO:",tso["DoubleTSO"]/tot; \
+print "Reads with barcode given polyA:", bc*100/polyA["poly_T_found"]; \
+print "Percent barcoded reads:", bc*100/tot; \
+print "Percent TSO found:",tso["TSO_found"]*100/tot; \
+print "Percent doubleTSO:",tso["DoubleTSO"]*100/tot; \
 print "Average barcoded read length:",len/bc; \
 print "Average read length of file:", totLen/tot;}' > $outFolder/"DeconvBC_"$f_name"_summary"
+
+
+num=$(ls $outFolder/"DeconvBC_"*"_summary" | wc -l);
+if [ "$num" -gt 1 ]
+then
+awk -v num=$num '{split($0,t,": "); a[FNR]+=$NF; title[FNR]=t[1];}END \
+{for(i=1;i<=FNR;i++)print title[i],":",a[i]/num;}' \
+$outFolder/"DeconvBC_"*"_summary" > $outFolder/AllFiles_Summary
+fi
